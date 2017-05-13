513c27c9b74e076d405382d162f90b7548fc64e4
Yuki Nagahara <nagahara.ubi@gmail.com>
2017-05-13 21:35:58 +0900

    練習4-3 32bit 64bit 両対応

--- ch04/ex03/reverse.go
+++ ch04/ex03/reverse.go
@@ -6,7 +6,11 @@ package reverse
 import "unsafe"
 
 const (
-	memAlign = 8
+	intSize = 4 << (^uint(0) >> 63)
+	// 32bit なら 0bit shiftで 4 のまま。 64bitなら1bit shiftして8になる。
+
+	// Goでintが32ビットか64ビットか調べる方法
+	// http://qiita.com/ruiu/items/28c77ed483cec365fe84
 )
 
 func reverse(s *[]int, l int) {
@@ -25,5 +29,5 @@ func getPointer(addr *[]int, suffix int) (result *int) {
 	// 2. 配列のアドレスへ足し算 address = uip + index
 	// 3. unsafe.Pointer化 unsp = unsafe.Pointer(address)
 	// 4. int アドレス型にキャスト return (*int)(unsp)
-	return (*int)(unsafe.Pointer(uip + uintptr(suffix*memAlign)))
+	return (*int)(unsafe.Pointer(uip + uintptr(suffix*intSize)))
 }

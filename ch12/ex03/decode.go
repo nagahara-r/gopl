edd029c0d115ea8b1bc65e2a96f405c6215e6c3e
Yuki Nagahara <nagahara.ubi@gmail.com>
2017-09-09 22:51:45 +0900

    練習12-3

---
+++ ch12/ex03/decode.go
@@ -0,0 +1,267 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Copyright © 2017 Yuki Nagahara
+// 練習12-3: float, complex, bool, chan, func, interface の実装をします。
+
+// See page 344.
+
+// Package sexpr provides a means for converting Go objects to and
+// from S-expressions.
+package sexpr
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+	"text/scanner"
+	"unsafe"
+)
+
+var reflectTypes = map[string]reflect.Type{
+	// "invalid":        reflect.TypeOf(nil),
+	"bool":       reflect.TypeOf(bool(true)),
+	"int":        reflect.TypeOf(int(0)),
+	"int8":       reflect.TypeOf(int8(0)),
+	"int16":      reflect.TypeOf(int16(0)),
+	"int32":      reflect.TypeOf(int32(0)),
+	"int64":      reflect.TypeOf(int64(0)),
+	"uint":       reflect.TypeOf(uint(0)),
+	"uint8":      reflect.TypeOf(uint8(0)),
+	"uint16":     reflect.TypeOf(uint16(0)),
+	"uint32":     reflect.TypeOf(uint32(0)),
+	"uint64":     reflect.TypeOf(uint64(0)),
+	"uintptr":    reflect.TypeOf(uintptr(0)),
+	"float32":    reflect.TypeOf(float32(0)),
+	"float64":    reflect.TypeOf(float64(0)),
+	"complex64":  reflect.TypeOf(complex64(0)),
+	"complex128": reflect.TypeOf(complex128(0)),
+	"string":     reflect.TypeOf(""),
+	"unsafe":     reflect.TypeOf(unsafe.Pointer(uintptr(0))),
+
+	// Slice は型が基本形であればSliceにすることでサポートする。
+	"[]bool":           reflect.TypeOf([]bool{}),
+	"[]int":            reflect.TypeOf([]int{}),
+	"[]int8":           reflect.TypeOf([]int8{}),
+	"[]int16":          reflect.TypeOf([]int16{}),
+	"[]int32":          reflect.TypeOf([]int32{}),
+	"[]int64":          reflect.TypeOf([]int64{}),
+	"[]uint":           reflect.TypeOf([]uint{}),
+	"[]uint8":          reflect.TypeOf([]uint8{}),
+	"[]uint16":         reflect.TypeOf([]uint16{}),
+	"[]uint32":         reflect.TypeOf([]uint32{}),
+	"[]uint64":         reflect.TypeOf([]uint64{}),
+	"[]uintptr":        reflect.TypeOf([]uintptr{}),
+	"[]float32":        reflect.TypeOf([]float32{}),
+	"[]float64":        reflect.TypeOf([]float64{}),
+	"[]complex64":      reflect.TypeOf([]complex64{}),
+	"[]complex128":     reflect.TypeOf([]complex128{}),
+	"[]string":         reflect.TypeOf([]string{}),
+	"[]unsafe.Pointer": reflect.TypeOf([]unsafe.Pointer{}),
+
+	// 以下は型へのマッピングが困難なため非サポート
+	// "array"
+	// "interface":      reflect.Interface,
+	// "map":            reflect.Map,
+	// "struct":         struct{},
+
+	// 以下はマーシャリングできない
+	// "chan":           reflect.Chan,
+	// "func":           reflect.Func,
+}
+
+//!+Unmarshal
+// Unmarshal parses S-expression data and populates the variable
+// whose address is in the non-nil pointer out.
+func Unmarshal(data []byte, out interface{}) (err error) {
+	lex := &lexer{scan: scanner.Scanner{Mode: scanner.GoTokens}}
+	lex.scan.Init(bytes.NewReader(data))
+	lex.next() // get the first token
+	defer func() {
+		// NOTE: this is not an example of ideal error handling.
+		if x := recover(); x != nil {
+			err = fmt.Errorf("error at %s: %v", lex.scan.Position, x)
+		}
+	}()
+	read(lex, reflect.ValueOf(out).Elem())
+	return nil
+}
+
+//!-Unmarshal
+
+//!+lexer
+type lexer struct {
+	scan  scanner.Scanner
+	token rune // the current token
+}
+
+func (lex *lexer) next()        { lex.token = lex.scan.Scan() }
+func (lex *lexer) text() string { return lex.scan.TokenText() }
+
+func (lex *lexer) consume(want rune) {
+	if lex.token != want { // NOTE: Not an example of good error handling.
+		panic(fmt.Sprintf("got %q, want %q", lex.text(), want))
+	}
+	lex.next()
+}
+
+//!-lexer
+
+// The read function is a decoder for a small subset of well-formed
+// S-expressions.  For brevity of our example, it takes many dubious
+// shortcuts.
+//
+// The parser assumes
+// - that the S-expression input is well-formed; it does no error checking.
+// - that the S-expression input corresponds to the type of the variable.
+// - that all numbers in the input are non-negative decimal integers.
+// - that all keys in ((key value) ...) struct syntax are unquoted symbols.
+// - that the input does not contain dotted lists such as (1 2 . 3).
+// - that the input does not contain Lisp reader macros such 'x and #'x.
+//
+// The reflection logic assumes
+// - that v is always a variable of the appropriate type for the
+//   S-expression value.  For example, v must not be a boolean,
+//   interface, channel, or function, and if v is an array, the input
+//   must have the correct number of elements.
+// - that v in the top-level call to read has the zero value of its
+//   type and doesn't need clearing.
+// - that if v is a numeric variable, it is a signed integer.
+
+//!+read
+func read(lex *lexer, v reflect.Value) {
+	switch lex.token {
+	case scanner.Ident:
+		// The only valid identifiers are
+		// "nil" and struct field names.
+		if lex.text() == "nil" {
+			v.Set(reflect.Zero(v.Type()))
+			lex.next()
+			return
+		} else if lex.text() == "false" || lex.text() == "true" {
+			b, _ := strconv.ParseBool(lex.text())
+			v.SetBool(b)
+			lex.next()
+			return
+		}
+	case scanner.String:
+		s, _ := strconv.Unquote(lex.text()) // NOTE: ignoring errors
+		v.SetString(s)
+		lex.next()
+		return
+	case '-':
+		lex.next()
+		setNumber("-"+lex.text(), lex, v)
+		lex.next()
+		return
+	case scanner.Int, scanner.Float:
+		setNumber(lex.text(), lex, v)
+		lex.next()
+		return
+	case '(':
+		lex.next()
+		readList(lex, v)
+		lex.next() // consume ')'
+		return
+	case '#':
+		lex.next() // C
+		if lex.text() == "C" {
+			lex.next() // (
+			lex.next() // 1.00000
+			real, _ := strconv.ParseFloat(lex.text(), 64)
+			lex.next() // ,
+			lex.next() // 2.00000
+			imag, _ := strconv.ParseFloat(lex.text(), 64)
+			v.SetComplex(complex(real, imag))
+
+			lex.next()
+		} else if lex.text() == "I" {
+			lex.next() // (
+			lex.next() // Type
+			readList(lex, v)
+		}
+		lex.next() // consume ')'
+		return
+	}
+	panic(fmt.Sprintf("unexpected token %q", lex.text()))
+}
+
+func setNumber(str string, lex *lexer, v reflect.Value) {
+	switch lex.token {
+	case scanner.Int:
+		i, _ := strconv.ParseInt(str, 0, 64) // NOTE: ignoring errors
+		//i, _ := strconv.Atoi(lex.text()) // NOTE: ignoring errors
+		v.SetInt(int64(i))
+	case scanner.Float:
+		f, _ := strconv.ParseFloat(lex.text(), 64) // NOTE: ignoring errors
+		v.SetFloat(f)
+	}
+}
+
+//!-read
+
+//!+readlist
+func readList(lex *lexer, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Array: // (item ...)
+		for i := 0; !endList(lex); i++ {
+			read(lex, v.Index(i))
+		}
+
+	case reflect.Slice: // (item ...)
+		for !endList(lex) {
+			item := reflect.New(v.Type().Elem()).Elem()
+			read(lex, item)
+			v.Set(reflect.Append(v, item))
+		}
+
+	case reflect.Struct: // ((name value) ...)
+		for !endList(lex) {
+			lex.consume('(')
+			if lex.token != scanner.Ident {
+				panic(fmt.Sprintf("got token %q, want field name", lex.text()))
+			}
+			name := lex.text()
+			lex.next()
+			read(lex, v.FieldByName(name))
+			lex.consume(')')
+		}
+
+	case reflect.Map: // ((key value) ...)
+		v.Set(reflect.MakeMap(v.Type()))
+		for !endList(lex) {
+			lex.consume('(')
+			key := reflect.New(v.Type().Key()).Elem()
+			read(lex, key)
+			value := reflect.New(v.Type().Elem()).Elem()
+			read(lex, value)
+			v.SetMapIndex(key, value)
+			lex.consume(')')
+		}
+
+	case reflect.Interface:
+		l := strings.Trim(lex.text(), "\"")
+		iv := reflect.New(reflectTypes[l]).Elem()
+
+		lex.next()
+		read(lex, iv)
+		v.Set(iv)
+
+	default:
+		panic(fmt.Sprintf("cannot decode list into %v", v.Type()))
+	}
+}
+
+func endList(lex *lexer) bool {
+	switch lex.token {
+	case scanner.EOF:
+		panic("end of file")
+	case ')':
+		return true
+	}
+	return false
+}
+
+//!-readlist
---
+++ ch12/ex03/sexpr_test.go
@@ -0,0 +1,170 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Copyright © 2017 Yuki Nagahara
+
+package sexpr
+
+import (
+	"math"
+	"reflect"
+	"testing"
+)
+
+// Test verifies that encoding and decoding a complex data value
+// produces an equal result.
+//
+// The test does not make direct assertions about the encoded output
+// because the output depends on map iteration order, which is
+// nondeterministic.  The output of the t.Log statements can be
+// inspected by running the test with the -v flag:
+//
+// 	$ go test -v gopl.io/ch12/sexpr
+//
+func Test(t *testing.T) {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+
+		Award      bool
+		ComplexNum complex64
+
+		Description interface{}
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+
+		Award:      true,
+		ComplexNum: 1 + 2i,
+
+		//Description: MovieDescription{"This is Description.", "This is Author"},
+		Description: "This is Description.",
+	}
+
+	// Encode it
+	data, err := Marshal(strangelove)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var movie Movie
+	if err := Unmarshal(data, &movie); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", movie)
+
+	// Check equality.
+	if !reflect.DeepEqual(movie, strangelove) {
+		t.Fatal("not equal")
+	}
+
+	// Pretty-print it:
+	data, err = MarshalIndent(strangelove)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("MarshalIdent() = %s\n", data)
+}
+
+func TestInterface(t *testing.T) {
+	type IfTest struct {
+		// Generics
+		Booli       interface{}
+		Inti        interface{}
+		Int8i       interface{}
+		Int16i      interface{}
+		Int32i      interface{}
+		Int64i      interface{}
+		Float32i    interface{}
+		Float64i    interface{}
+		Complex64i  interface{}
+		Complex128i interface{}
+		Stringi     interface{}
+
+		// Slices
+		Boolsi       interface{}
+		Intsi        interface{}
+		Int8si       interface{}
+		Int16si      interface{}
+		Int32si      interface{}
+		Int64si      interface{}
+		Float32si    interface{}
+		Float64si    interface{}
+		Complex64si  interface{}
+		Complex128si interface{}
+		Stringsi     interface{}
+	}
+	testdata := IfTest{
+		Booli:       bool(true),
+		Inti:        int(0),
+		Int8i:       int8(math.MaxInt8),
+		Int16i:      int16(math.MaxInt16),
+		Int32i:      int32(math.MaxInt32),
+		Int64i:      int64(math.MaxInt64),
+		Float32i:    float32(math.MaxFloat32),
+		Float64i:    float64(math.MaxFloat64),
+		Complex64i:  complex64(complex(math.MaxFloat32, math.MaxFloat32)),
+		Complex128i: complex(math.MaxFloat64, math.MaxFloat64),
+		Stringi:     "This is String",
+
+		Boolsi:       []bool{bool(false), bool(true)},
+		Intsi:        []int{int(0), int(1)},
+		Int8si:       []int8{int8(math.MinInt8), int8(math.MaxInt8)},
+		Int16si:      []int16{int16(math.MinInt16), int16(math.MaxInt16)},
+		Int32si:      []int32{int32(math.MinInt32), int32(math.MaxInt32)},
+		Int64si:      []int64{int64(math.MinInt64), int64(math.MaxInt64)},
+		Float32si:    []float32{float32(0), float32(math.MaxFloat32)},
+		Float64si:    []float64{float64(0), float64(math.MaxFloat64)},
+		Complex64si:  []complex64{complex64(complex(0, 0)), complex64(complex(math.MaxFloat32, math.MaxFloat32))},
+		Complex128si: []complex128{complex(0, 0), complex(math.MaxFloat64, math.MaxFloat64)},
+		Stringsi:     []string{"This is String 1.", "文字列2です。"},
+	}
+
+	// Encode it
+	data, err := Marshal(testdata)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var ifTest IfTest
+	if err := Unmarshal(data, &ifTest); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal()  = %+v\n", ifTest)
+	t.Logf("OriginalData = %+v\n", testdata)
+
+	// Check equality.
+	if !reflect.DeepEqual(ifTest, testdata) {
+		t.Fatal("not equal")
+	}
+
+	// Pretty-print it:
+	data, err = MarshalIndent(testdata)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("MarshalIdent() = %s\n", data)
+}

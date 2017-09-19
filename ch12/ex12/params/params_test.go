d49ef87c0e601690acc93ba46f491451a463a21a
Yuki Nagahara <nagahara.ubi@gmail.com>
2017-09-20 00:23:01 +0900

    練習12-12 テストコード修正

--- ch12/ex12/params/params_test.go
+++ ch12/ex12/params/params_test.go
@@ -3,54 +3,52 @@
 package params
 
 import (
-	"log"
-	"strings"
+	"net/http"
+	"reflect"
 	"testing"
 )
 
-func TestPack(t *testing.T) {
+func TestPackAndUnpack(t *testing.T) {
 	url := "http://localhost:12345/search"
 
 	type S struct {
 		ZipCode       int    `http:"zcode,zipcode"`
 		MailAddress   string `http:"mail,mailaddress"`
 		CreditCardNum string `http:"credit,creditcard"`
+		Other         []string
 	}
 	tests := []struct {
-		str      S
-		expected []string
-		err      error
+		str S
+		err error
 	}{
 		{
-			S{2430465, "test@example.com", "1234567890123456"},
-			[]string{"mail=test@example.com", "zcode=2430465", "credit=1234567890123456"},
+			S{2430465, "test@example.com", "1234567890123456", []string{"Other Message1", "Message2"}},
 			nil,
 		}, {
-			S{1, "test@example.com", "1234567890123456"},
-			nil,
+			S{1, "test@example.com", "1234567890123456", []string{""}},
 			ErrZip,
 		}, {
-			S{1234567, "testexample", "1234567890123456"},
-			nil,
+			S{1234567, "testexample", "1234567890123456", []string{""}},
 			ErrMailAddress,
 		}, {
-			S{1234567, "test@example.com", "12345678901234567"},
-			nil,
+			S{1234567, "test@example.com", "12345678901234567", []string{""}},
 			ErrCreditCardNumber,
 		}, {
-			S{1234567, "", "1234567890123456"},
-			[]string{"zcode=1234567", "credit=1234567890123456"},
+			S{1234567, "", "1234567890123456", []string{""}},
 			nil,
 		}, {
-			S{1234567, "test@example.com", ""},
-			[]string{"zcode=1234567", "mail=test@example.com"},
+			S{1234567, "test@example.com", "", []string{""}},
+			nil,
+		}, {
+			S{2430465, "test@example.com", "1234567890123456", nil},
 			nil,
 		},
 	}
 
 	for _, test := range tests {
-		result, err := Pack(url, &test.str)
+		structToURL, err := Pack(url, &test.str)
 
+		// 期待結果がエラーの場合は、エラーが期待通りか確認します
 		if err != nil {
 			if err == test.err {
 				continue
@@ -58,26 +56,22 @@ func TestPack(t *testing.T) {
 			t.Errorf("%v", err)
 		}
 
-		if !isExpected(result, url, test.expected) {
-			t.Errorf("want = %v, result = %v", test.expected, result)
+		// HTTPリクエストにします
+		var result S
+		req, err := http.NewRequest("GET", structToURL, nil)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+
+		// HTTPリクエストをアンパックします
+		err = Unpack(req, &result)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+
+		// 元の構造体と同じか確認します
+		if !reflect.DeepEqual(test.str, result) {
+			t.Errorf("original(expected) = %v, result = %v", test.str, result)
 		}
 	}
-}
-
-func isExpected(result string, expectedurl string, expectedparams []string) bool {
-	// URLでスタートするか
-	if !strings.HasPrefix(result, expectedurl) {
-		log.Printf("Has No Url = %v\n", result)
-		return false
-	}
-
-	// すべての予期した要素を含むか
-	for _, expectedparam := range expectedparams {
-		if !strings.Contains(result, expectedparam) {
-			log.Printf("Has No Param = %v\n", expectedparam)
-			return false
-		}
-	}
-
-	return true
 }
